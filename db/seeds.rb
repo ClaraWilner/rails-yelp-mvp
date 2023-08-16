9f91aa1165961f96c216fb5b615b30ac0c84058b
clara wilner <clara.luna.wilner@gmail.com>
2023-08-16 16:46:30 +0200

    ex 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,19 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+puts 'Cleaning database...'
+Restaurant.destroy_all
+puts 'Creating restaurants...'
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.create(
+  [
+    { name: 'Epicure', category: 'french', address: '75008 Paris' },
+    { name: 'Merguez', category: 'french', address: '75009 Paris' },
+    { name: 'Falafel', category: 'chinese', address: '75010 Paris' },
+    { name: 'Surpriz', category: 'chinese', address: '75011 Paris' },
+    { name: 'Kebab', category: 'japanese', address: '75012 Paris' }
+  ]
+)
+puts 'finished!'
